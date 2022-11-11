a5298cb0f4996bd5b07c15a4f6c7649bb23ac368
tourniquet <admyn3d@gmail.com>
2022-11-11 23:55:30 +0000

    added Logs app urls

---
+++ learning_logs/urls.py
@@ -0,0 +1,10 @@
+"""Defining URL patterns for learning_logs"""
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index')
+]
