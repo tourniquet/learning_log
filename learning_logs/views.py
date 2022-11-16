dbb128548a2ab1c7e1159223db8397a3d19a0484
tourniquet <admyn3d@gmail.com>
2022-11-16 17:56:10 +0000

    created view and template for all topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Topic
 
 def index(request):
   """The home page for Learning Log"""
   return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+  """Show all topics"""
+  topics = Topic.objects.order_by('date_added')
+  context = {'topics': topics}
+  return render(request, 'learning_logs/topics.html', context)
+
+
