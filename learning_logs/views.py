935aa901fb9d73b686e98e8bd1661570c52ce9a9
tourniquet <admyn3d@gmail.com>
2022-11-16 17:57:30 +0000

    created view and template for creating a new topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
   """The home page for Learning Log"""
@@ -23,3 +23,18 @@ def topic(request, topic_id):
   return render(request, 'learning_logs/topic.html', context)
 
 
+def new_topic(request):
+  """Add a new topic"""
+  if request.method != 'POST':
+    # No data submitted, create a blank form
+    form = TopicForm()
+  else:
+    # POST data submitted, process data
+    form = TopicForm(data=request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('learning_logs:topics')
+
+  # Display a blank or invalid form
+  context = {'form': form}
+  return render(request, 'learning_logs/new_topic.html', context)
